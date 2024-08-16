730eb7fc1aeeeb977629d9ce4957a40af5803050
Francisco Uncal <franciscouncal@Fran-MacBook-Pro.local>
2024-08-16 10:57:51 -0300

    Agregando stilos al footer

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,65 @@ cerrar.addEventListener("click", () => {
   nav.classList.remove("visible");
 });
 
+// Scroll
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll("section");
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        } else {
+          entry.target.classList.remove("visible");
+        }
+      });
+    },
+    { threshold: 1.0 }
+  );
+
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+});
+
+// Formualario
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log("Formualario enviado");
+
+    let form = event.target;
+    let feedback = document.getElementById("formFeedback");
+
+    fetch(form.action, {
+      method: "POST",
+      body: new FormData(form),
+    })
+      .then((response) => {
+        if (response.ok) {
+          Swal.fire({
+            icon: "success",
+            title: "¡Éxito!",
+            text: "Gracias por tu mensaje. Nos pondremos en contacto contigo pronto.",
+            confirmButtonText: "Aceptar",
+          });
+          form.reset();
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Hubo un error al enviar tu mensaje. Por favor, inténtalo de nuevo.",
+            confirmButtonText: "Aceptar",
+          });
+        }
+      })
+      .catch((error) => {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Hubo un error al enviar tu mensaje. Por favor, inténtalo de nuevo.",
+          confirmButtonText: "Aceptar",
+        });
+      });
+  });
